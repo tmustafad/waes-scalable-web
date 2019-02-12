30cf2fc645cf525a5b52f792139f548e1ae384cb
Rodrigo Souza <rsouza01@gmail.com>
2019-02-12 20:12:11 -0200

    First commit

---
+++ src/main/java/com/rsouza01/waesscalableweb/WaesScalableWebApplication.java
@@ -0,0 +1,14 @@
+package com.rsouza01.waesscalableweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaesScalableWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaesScalableWebApplication.class, args);
+	}
+
+}
+
